3edc7a6263374caf68d3a5b3814896b9e2f4daec
von <fgq@stu.jnu.edu.cn>
2020-06-02 15:39:39 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
-# 首先要检查是否安装bbr（lsmod | grep bbr），是的话直接开始安装运行screen
-yum install updata && yum install wget
+yum -y install updata
+#检查wget是否安装
+rpm -q wget > /dev/null
+if [ $? -eq 0 ];then
+echo wget已安装!
+else
+yum -y install wget
+fi
+#检查vim是否安装
+rpm -q vim > /dev/null
+if [ $? -eq 0 ];then
+echo vim已安装!
+else
+yum -y install vim
+fi
+#检查screen是否安装
+rpm -q screen > /dev/null
+if [ $? -eq 0 ];then
+echo screen已安装!
+else
+yum -y install screen
+fi
+# 首先要检查是否安装bbr
+lsmod | grep bbr > /dev/null
+if [ $? -eq 0 ];then
+echo bbr已安装
+else
 wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && bash bbr.sh           # 此处会重启
-lsmod | grep bbr                 # 检查bbr是否启动
-# 根据上一指令的输出结果判断：成功的话执行yum install screen && screen -S lnmp，失败的话输出错误并退出脚本
+fi
+lsmod | grep bbr > /dev/null
+if [ $? -eq 0 ];then
+screen -S lnmp
 wget http://soft.vpser.net/lnmp/lnmp1.6.tar.gz -cO lnmp1.6.tar.gz && tar zxf lnmp1.6.tar.gz && cd lnmp1.6 && ./install.sh lnmp 
-wget https://install.direct/go.sh && sudo bash go.sh  
+else
+echo 请重新运行脚本安装bbr && exit
+if
+wget https://install.direct/go.sh && sudo bash go.sh
+read -p "请输入域名" domain
+read -p "qi"
+#生成uuid：cat /proc/sys/kernel/random/uuid
 # read命令写入域名、反代域名，v2的id、传输协议、路径
 # 根据read的变量将相关内容写入配置文件。
 systemctl restart v2ray  
