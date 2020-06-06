de41f921f0ef1247ceba298e0fddb33b05a3a036
von <fgq@stu.jnu.edu.cn>
2020-06-07 00:32:33 +0800

    12

--- myang.sh
+++ myang.sh
@@ -8,17 +8,17 @@ sed -i '112s/#//g' /etc/ssh/sshd_config
 sed -i '113s/#//g' /etc/ssh/sshd_config
 service sshd restart
 yum -y install updata                                                                            #安装更新
-rpm -q git >/dev/null && echo "git已安装." || yum -y install git                                  #安装git
-rpm -q wget >/dev/null && echo "wget已安装." || yum -y install wget                               #安装wget
-rpm -q fuse >/dev/null && echo "fuse已安装." || yum -y install fuse                               #安装fuse
-rpm -q lrzsz >/dev/null && echo "lrzsz已安装." || yum -y install lrzsz                            #安装lrzsz
-rpm -q unzip >/dev/null && echo "unzip已安装." || yum -y install unzip                            #安装unzip
-rpm -q screen >/dev/null && echo "screen已安装." || yum -y install screen                         #安装screen
-rpm -q python3 >/dev/null && echo "python3已安装." || yum -y install python3                      #安装python3
-rpm -q python3-pip >/dev/null && echo "python3-pip已安装." || yum -y install python3-pip          #安装python3-pip
+rpm -q git &>/dev/null && echo "git已安装." || yum -y install git                                  #安装git
+rpm -q wget &>/dev/null && echo "wget已安装." || yum -y install wget                               #安装wget
+rpm -q fuse &>/dev/null && echo "fuse已安装." || yum -y install fuse                               #安装fuse
+rpm -q lrzsz &>/dev/null && echo "lrzsz已安装." || yum -y install lrzsz                            #安装lrzsz
+rpm -q unzip &>/dev/null && echo "unzip已安装." || yum -y install unzip                            #安装unzip
+rpm -q screen &>/dev/null && echo "screen已安装." || yum -y install screen                         #安装screen
+rpm -q python3 &>/dev/null && echo "python3已安装." || yum -y install python3                      #安装python3
+rpm -q python3-pip &>/dev/null && echo "python3-pip已安装." || yum -y install python3-pip          #安装python3-pip
 [ -e /usr/bin/gclone ] && echo "gclone已安装." || bash <(wget -qO- https://git.io/gclone.sh)      #安装gclone
 [ -e /usr/bin/rclone ] && echo "gclone已重命名." || cp /usr/bin/gclone /usr/bin/rclone            #重命名gclone
-git clone https://github.com/F-gq/AutoRclone.git && \mv -f /root/AutoRclone/rclone.conf /root/.config/rclone/    #安装AutoRclone
+git clone https://github.com/F-gq/AutoRclone.git;\mv -f /root/AutoRclone/rclone.conf /root/.config/rclone/    #安装AutoRclone
 if [ -e /usr/local/bin/aria2c ]; then                                                            #检查aria2是否安装
     echo aria2已安装!
 else
