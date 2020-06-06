645052694a18e88580a46884b4aabfa70fd7c26e
von <fgq@stu.jnu.edu.cn>
2020-06-06 22:47:56 +0800

    12

--- v2ray.sh
+++ v2ray.sh
@@ -8,7 +8,7 @@ sed -i '112s/#//g' /etc/ssh/sshd_config
 sed -i '113s/#//g' /etc/ssh/sshd_config
 service sshd restart
 yum -y install updata
-wget https://raw.githubusercontent.com/F-gq/stools/master/domain.conf
+[ -e /root/]wget https://raw.githubusercontent.com/F-gq/stools/master/domain.conf
 wget https://raw.githubusercontent.com/F-gq/stools/master/v2ray.json
 lsmod | grep bbr > /dev/null
 if [ $? -eq 0 ];then
@@ -26,10 +26,10 @@ read -p "请输入端口：" port
 id=$(cat /proc/sys/kernel/random/uuid)
 sed -i "s/\$domain/$domain/g" /root/domain.conf
 sed -i "s/\$proxydomain/$proxydomain/g" /root/domain.conf
-sed -i "s|path|$path|g" /root/domain.conf                      #解决sed中环境变量替换
+sed -i "s|path1|$path|g" /root/domain.conf                      #解决sed中环境变量替换
 sed -i "s/\$port/$port/g" /root/domain.conf
 sed -i "s/\$port/$port/g" /root/v2ray.json
-sed -i "s|path|$path|g" /root/v2ray.json
+sed -i "s|path1|$path|g" /root/v2ray.json
 sed -i "s/\$id/$id/g" /root/v2ray.json
 sed -i "s/\$alterid/$alterid/g" /root/v2ray.json
 \mv -f /root/domain.conf "/usr/local/nginx/conf/vhost/$domain.conf"
