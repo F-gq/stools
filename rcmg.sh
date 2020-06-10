46a66402b73e419008cd974ba79fb6b54b0f3b23
von <fgq@stu.jnu.edu.cn>
2020-06-10 12:49:16 +0800

    13

--- rcmg.sh
+++ rcmg.sh
@@ -10,8 +10,8 @@ if [ $num -eq 1 ]; then
     gclone delete $name: --drive-trashed-only --drive-use-trash=false --verbose=2 --fast-list &
 elif [ $num -eq 2 ]; then
     read -p "请输入gclone配置的网盘名称：" name1
-    mkdir -p /home/$name1
-    rclone mount $name1: /home/$name1 --allow-other --allow-non-empty --vfs-cache-mode writes &
+    mkdir -p /mnt/$name1
+    rclone mount $name1: /mnt/$name1 --allow-other --allow-non-empty --vfs-cache-mode writes &
     sleep 5 && df -h
 elif [ $num -eq 3 ]; then
     read -p "请输入文件夹路径:" dirpath
@@ -29,27 +29,27 @@ elif [ $num -eq 5 ]; then
 elif [ $num -eq 6 ]; then
     read -p "请输入文件夹路径：" dirpath
     filename="${dirpath##*/}"
-    nohup zip -r /home/aabc/compression/$filename.zip $dirpath &>/dev/null &
+    nohup zip -r /mnt/aabc/compression/$filename.zip $dirpath &>/dev/null &
 elif [ $num -eq 7 ]; then
     echo -e "\e[1;33m注意只会批量压缩指定目录下的一级子文件夹，二级以后不会压缩，压缩前请将文件夹整理规范。\e[0m"
     read -p "请输入文件夹路径：" dirpath
-    nohup ls -Al $dirpath | grep "^d" | awk '{print $9}' | xargs -i zip -r /home/aabc/compression/{}.zip $dirpath/{} &>/dev/null &
+    nohup ls -Al $dirpath | grep "^d" | awk '{print $9}' | xargs -i zip -r /mnt/aabc/compression/{}.zip $dirpath/{} &>/dev/null &
 elif [ $num -eq 8 ]; then
     read -p "请输入文件（夹）路径：" path && export path
     if [ -d $path ]; then
-        nohup ls -Al $path | grep -E '.*zip|.*rar|.*tar|.*gz|.*7z' | awk '{print $9}' | xargs -i unar -f $path/{} -o /home/virginia/已解压/12 &>/dev/null &
+        nohup ls -Al $path | grep -E '.*zip|.*rar|.*tar|.*gz|.*7z' | awk '{print $9}' | xargs -i unar -f $path/{} -o /mnt/virginia/已解压/12 &>/dev/null &
     else
         read -p "请输入指定编码方式（乱码时使用，无则回车）：" encode
         if [ -z "$encode" ]; then
-            nohup unar $path -o /home/virginia/已解压/12 &
+            nohup unar $path -o /mnt/virginia/已解压/12 &
             echo -e "\e[1;32m请先输入回车，再输入tail -f nohup.out查看解压详情\e[0m"
         else
-            nohup unar -e $encode $path -o /home/virginia/已解压/12 &
+            nohup unar -e $encode $path -o /mnt/virginia/已解压/12 &
             echo -e "\e[1;32m请先输入回车，再输入tail -f nohup.out查看解压详情\e[0m"
         fi
     fi
 elif [ $num -eq 9 ]; then
-    read -p "请输入gclone配置的网盘名称：" name && fusermount -qzu /home/$name
+    read -p "请输入gclone配置的网盘名称：" name && fusermount -qzu /mnt/$name
 elif [ $num -eq 10 ]; then
     read -p "请输入压缩包路径：" path1
     echo -e "\e[1;32m请输入指定编码，常用编码有UTF-8、GBK、GB18030、BIG5、GB2321。\e[0m"
