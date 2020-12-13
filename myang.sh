8ef57e977e2c4dfa031bbbddee589db731da0171
von <fgq@stu.jnu.edu.cn>
2020-12-13 23:21:05 +0800

    111

--- myang.sh
+++ myang.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/bash
 #aria2 and gclone.Use aria2 and gclone to download,manage and copy files.
-export copy.sh=/root/AutoRclone/copy.sh
-export rcmg.sh=/root/AutoRclone/rcmg.sh
 mkdir /data
-yum -y install updata                                                                   #安装更新
-rpm -q git &>/dev/null && echo -e "\e[1;32mgit已安装.\e[0m" || yum -y install git                                  #安装git
-rpm -q wget &>/dev/null && echo -e "\e[1;32mwget已安装.\e[0m" || yum -y install wget                               #安装wget
-rpm -q fuse &>/dev/null && echo -e "\e[1;32mfuse已安装.\e[0m" || yum -y install fuse                               #安装fuse
-rpm -q unzip &>/dev/null && echo -e "\e[1;32munzip已安装.\e[0m" || yum -y install unzip                            #安装unzip
-rpm -q screen &>/dev/null && echo -e "\e[1;32mscreen已安装.\e[0m" || yum -y install screen                         #安装screen
+#!/usr/bin/bash
+#生成磁力链接写入文本/root/.aria2c/aria2.session
+soft=(upgrade update git wget curl fuse lrzsz unzip screen python3 python3-pip epel-release unar vim)
+
+yum clean all && yum makecache && yum -y install ${soft[*]}
+lsmod | grep bbr &> /dev/null && echo -e "\e[1;32mbbr已安装\e[0m" || curl https://raw.githubusercontent.com/teddysun/across/master/bbr.sh | bash
 [ -e /usr/bin/gclone ] && echo -e "\e[1;32mgclone已安装.\e[0m" || bash <(wget -qO- https://git.io/gclone.sh)      #安装gclone
-[ -e /usr/bin/rclone ] && echo -e "\e[1;32mgclone已重命名.\e[0m" || cp /usr/bin/gclone /usr/bin/rclone            #重命名gclone
+[ -e /usr/bin/rclone ] && echo -e "\e[1;32mgclone已重命名.\e[0m" || cp /usr/bin/gclone /usr/bin/rclone
 git clone https://github.com/F-gq/AutoRclone.git;\mv -f /root/AutoRclone/rclone.conf /root/.config/rclone/     #安装相关软件、配置
 [ -e /usr/local/bin/aria2c ] && echo -e "\e[1;32maria2已安装。\e[0m" || wget https://raw.githubusercontent.com/P3TERX/aria2.sh/master/aria2.sh
 chmod u+x /root/aria2.sh && bash aria2.sh
@@ -19,7 +17,7 @@ chmod u+x /root/aria2.sh && bash aria2.sh
 chmod a+x /root/.aria2c/autoupload.sh
 echo -ne "\e[1;33m请输入用gclone设置的挂载名称:\e[0m" && read name
 echo -e "\e[1;32m请输入上传路径，例如：根目录填/，根目录下的1文件夹输入/1.以此类推。\e[0m"
-echo -ne "\e[1;33m请输入上传路径：\e[0m" && read path
+echo -ne "\e[1;33m请输入上传路径：\e[0m" && read path              #安装完毕还需手动更改aria配置文件和autoupload脚本
 mkdir -p /mnt/$name
 rclone mount $name: /mnt/$name --allow-other --allow-non-empty --vfs-cache-mode writes &        #挂载Googledrive
 service aria2 restart
